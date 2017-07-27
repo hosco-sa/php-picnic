8c4e30dc60402259276698d7050fa464c1ffd804
Pablo del Vecchio <pablodv@gmail.com>
2017-07-27 11:25:12 +0200

    added folder param on couchbase bucket dump

--- src/SimpleCouchbase.php
+++ src/SimpleCouchbase.php
@@ -2,6 +2,10 @@
 
 namespace Picnic;
 
+use CouchbaseN1qlQuery;
+use CouchbaseException;
+use ElasticSearch\Exception;
+
 class SimpleCouchbase {
 
     public $cluster;
@@ -130,5 +134,73 @@ class SimpleCouchbase {
 
         }
     }
-    
+
+    public function dumpAllKeysBeerSample()
+    {
+        // $queryI = CouchbaseN1qlQuery::fromString('CREATE PRIMARY INDEX `beer-sample-primary-index` ON `beer-sample` USING GSI;');
+
+        // $resultI = $this->cluster->openBucket($this->bucket)->query($queryI);
+
+        // print_r($resultI);
+
+        $queryS = CouchbaseN1qlQuery::fromString('SELECT * FROM `beer-sample` LIMIT 10');
+
+        $resultS = $this->cluster->openBucket($this->bucket)->query($queryS);
+
+        // print_r($resultS);
+
+        foreach ($resultS as $row) {
+            // print_r($row);
+
+            $json = json_encode($row, JSON_UNESCAPED_SLASHES);
+
+            echo $json."\n";
+        }
+    }
+
+    public function createPrimaryIndex($index='admin_fly_primary_index', $bucket='admin_fly')
+    {
+        try {
+            $queryI = CouchbaseN1qlQuery::fromString("CREATE PRIMARY INDEX `$index` ON `$bucket` USING GSI;");
+
+            $resultI = $this->cluster->openBucket($this->bucket)->query($queryI);
+
+            print_r($resultI);
+
+        } catch (CouchbaseException $e) {
+            echo $e->getMessage()."\n";
+        }
+    }
+
+    public function dumpAllKeys($bucket, $limit=1000, $offset=0, $folder='storage')
+    {
+        try {
+            $queryS = CouchbaseN1qlQuery::fromString("SELECT * FROM `$bucket` LIMIT $limit OFFSET $offset");
+
+            $resultS = $this->cluster->openBucket($this->bucket)->query($queryS);
+
+            // print_r($resultS);
+
+            $n = 0;
+
+            foreach ($resultS as $row) {
+                if (isset($row->admin_fly->uuid)) {
+                    $json = json_encode($row, JSON_UNESCAPED_SLASHES);
+
+                    echo $n++.": ".$row->admin_fly->uuid."\n";
+
+                    file_put_contents($folder.'/'.$row->admin_fly->uuid.'.json', $json);
+                } else {
+                    $json = json_encode($row, JSON_UNESCAPED_SLASHES);
+
+                    echo $json."\n";
+                }
+            }
+
+        } catch (CouchbaseException $e) {
+            echo $e->getMessage()."\n";
+        }
+
+
+    }
 }
